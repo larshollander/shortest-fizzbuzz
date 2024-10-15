ee59a8de5b2716692b98fc6f53e25b4f6b367ab2
Lars Hollander <larshollander@proton.me>
2024-10-15 12:50:08 +0200

    hardly remember how this thing works

--- Assembly.s
+++ Assembly.s
@@ -4,10 +4,10 @@ mov bx,%1
 mov dx,0
 div bx
 cmp dx,0
-jne %2
+jg %2
 sub di,4
 mov edx,%3
-mov [edi],edx
+mov[edi],edx
 %endmacro
 section .bss
 b RESQ 9
@@ -15,14 +15,14 @@ section .text
 global _start
 _start:mov cx,1
 i:lea edi,[b]
-mov [edi],rax
+mov[edi],rax
 add di,8
 mov byte[edi],10
 a 5,f,"Buzz"
 f:a 3,n,"Fizz"
 n:sub di,8
 cmp edi,b
-jne s
+jl s
 mov ax,cx
 add di,8
 c:dec di
@@ -30,19 +30,19 @@ mov dx,0
 mov bx,10
 div bx
 add dl,48
-mov [edi],dl
+mov[edi],dl
 cmp ax,0
-jne c
+jg c
 s:push rcx
 mov ax,4
 mov bx,1
 lea ecx,[edi]
 lea edx,[b+9]
-sub edx,ecx
+sub dx,cx
 int 80h
 pop rcx
 inc cx
 cmp cx,101
-jne i
+jl i
 mov ax,1
 int 80h
